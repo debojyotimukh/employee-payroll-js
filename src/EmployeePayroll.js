ac09185d6389955a0dd46c6ef1493dc10709e292
Debojyoti Mukherjee <debojyotimukh@gmail.com>
2020-11-07 03:34:30 -0800

    add max days and max hours condition

--- src/EmployeePayroll.js
+++ src/EmployeePayroll.js
@@ -1,9 +1,11 @@
-//uc4
+//uc5
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const MAX_HRS_IN_MONTH = 100;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -17,13 +19,14 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
-let empHrs = 0;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
 
-const NUM_OF_WORKING_DAYS=20;
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-    let empCheck=Math.floor(Math.random()*10)%3;
-    empHrs+=getWorkingHours(empCheck);   
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
 }
 
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp WAge: " + empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("UC5-- Total days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wages: " + empWage);
