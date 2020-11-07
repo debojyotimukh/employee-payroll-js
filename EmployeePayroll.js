01a2a322a0eb8604be86d792d69c9b231728c88e
Debojyoti Mukherjee <debojyotimukh@gmail.com>
2020-11-07 03:16:44 -0800

    refactor to add fuction to calculate hours worked

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -20,16 +20,22 @@ const WAGE_PER_HOUR = 20;
 
 let empHrs = 0;
 empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs = 0;
-}
+empHrs = getWorkingHours(empCheck);
 
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp WAge: " + empWage);
+
+//uc3
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+            break;
+        default:
+            return 0;
+    }
+}
+
