d3ee64869375f7f42a9a70910f84ac338088beec
Debojyoti Mukherjee <debojyotimukh@gmail.com>
2020-11-15 22:53:53 +0530

    create new payroll object on submit

--- scripts/employee-form.js
+++ scripts/employee-form.js
@@ -23,3 +23,47 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
 });
+
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+    } catch (error) {
+        return;
+    }
+};
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    try {
+        employeePayrollData._name = getInputValueById('#name');
+    } catch (error) {
+        setTextValue('.text-error', error);
+        throw error;
+    }
+    employeePayrollData._profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData._gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData._department = getSelectedValues('[name=department]');
+    employeePayrollData._salary = getInputValueById('#salary');
+    employeePayrollData._note = getInputValueById('#notes');
+    employeePayrollData._date = getInputValueById('#startDate');
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+};
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach(item => {
+        if (item.checked) selItems.push(item.value);
+    });
+};
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+};
+
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+};
